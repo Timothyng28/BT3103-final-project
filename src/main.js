5271dfc77fe809aa52a66c11ca42a5325d3b42c4
tmotluke <timothyng28@gmail.com>
2025-02-26 23:10:04 +0800

    feat: integrate Vue3 Particles into main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import './assets/main.css'
 import router from './router'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Particles from 'vue3-particles'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Particles).mount('#app')
