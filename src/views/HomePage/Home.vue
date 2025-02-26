5e14199c47808bbf7b8c71e51c155fd79b7b4a8a
tmotluke <timothyng28@gmail.com>
2025-02-26 23:11:05 +0800

    feat: Add background to Home Page

--- src/views/HomePage/Home.vue
+++ src/views/HomePage/Home.vue
@@ -1,6 +1,113 @@
 <template>
-Hi
-</template>
-
-<script setup>
-</script>
+    <div class="relative h-screen w-full flex items-center justify-center">
+      <Particles
+        id="sparkles"
+        class="absolute inset-0 z-10"
+        :particlesInit="particlesInit"
+        :options="particlesOptions"
+      />
+      <div class="relative z-20 text-center text-white -mt-40 justify-center items-center flex flex-col">
+        <h1 class = "text-6xl font-bold select-none">Countdown to the start of IHG24/25</h1>
+        <br>
+        <br>
+        <!--
+        <CountDown />
+        -->
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue';
+  import { loadSlim } from 'tsparticles-slim';
+  // import CountDown from './Countdown.vue';
+  
+  const particlesInit = async (engine) => {
+    await loadSlim(engine);
+  };
+  
+  const particlesOptions = {
+    background: {
+      color: {
+        value: "#000000",
+      },
+    },
+    fullScreen: {
+      enable: false,
+      zIndex: 1,
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: false,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      opacity: {
+        value: {
+          min: 0.1,
+          max: 1,
+        },
+        animation: {
+          enable: true,
+          speed: 4,
+          startValue: "random",
+          destroy: "none",
+        },
+      },
+      size: {
+        value: {
+          min: 1,
+          max: 3,
+        },
+      },
+      move: {
+        enable: true,
+        speed: {
+          min: 0.1,
+          max: 1,
+        },
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 120,
+      },
+      shape: {
+        type: "circle",
+      },
+    },
+    detectRetina: true,
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    text-shadow: 0 0 8px rgba(0, 0, 0, 0.7);
+  }
+  p {
+    text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  }
+  </style>
