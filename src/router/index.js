f2e79103e0cd62a8eff5a88be90efee9e03c52c3
tmotluke <timothyng28@gmail.com>
2025-02-26 22:57:19 +0800

    feat(router): set up Vue Router with basic directories and add page with basic vue boilerplates

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,31 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/HomePage/Home.vue')
+    },
+    {
+        path: '/games',
+        name: 'GamesPage',
+        component: () => import('@/views/GamesPage/GamesPage.vue')
+    },
+    {
+        path: '/login',
+        name: 'LoginPage',
+        component: () => import('@/views/LoginPage/LoginPage.vue')
+    },
+    {
+        path: '/scorekeeperDashboard',
+        name: 'ScorekeeperDashboard',
+        component: () => import('@/views/Dashboards/ScoreKeeperDashboard.vue')
+    },
+    {
+        path: '/residences',
+        name: 'ResidencesPage',
+        component: () => import('@/views/ResidencesPage/ResidencesPage.vue')
+    }
 ]
 
 const router = createRouter({
