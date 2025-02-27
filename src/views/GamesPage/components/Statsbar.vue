09e3bed285db5c9fdf347477da38bf05adfe86ca
tmotluke <timothyng28@gmail.com>
2025-02-27 09:36:33 +0800

    feat: update Statebar in Games Page

--- src/views/GamesPage/components/Statsbar.vue
+++ src/views/GamesPage/components/Statsbar.vue
@@ -1,6 +1,24 @@
 <template>
-statsbar
-</template>
-
-<script>
-</script>
+    <div class="stats stats-vertical shadow">
+      <div class="stat">
+        <div class="stat-title">Minutes Played</div>
+        <div class="stat-value">31K</div>
+      </div>
+    
+      <div class="stat">
+        <div class="stat-title">Medals Won</div>
+        <div class="stat-value">4,200</div>
+      </div>
+    
+      <div class="stat">
+        <div class="stat-title">Matches remaining</div>
+        <div class="stat-value">1,200</div>
+      </div>
+    </div>
+    </template>
+    
+    <script>
+    export default {
+      name: 'Statsbar',
+    }
+    </script>
