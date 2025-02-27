21c09214e4669d3009112e9bcc76479502725d99
tmotluke <timothyng28@gmail.com>
2025-02-27 09:37:50 +0800

    feat: update Sidebar in Games Page

--- src/views/GamesPage/components/SideBar.vue
+++ src/views/GamesPage/components/SideBar.vue
@@ -1,6 +1,40 @@
 <template>
-sideBar
-</template>
-
-<script>
-</script>
+    <ul class="menu bg-base-200 rounded-box w-40">
+        <!-- replace with v-for loop when actually implementing -->
+      <li>
+        <details>
+          <summary>Football</summary>
+          <ul>
+            <li><a>Male</a></li>
+            <li><a>Female</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary>Basketball</summary>
+          <ul>
+            <li><a>Male</a></li>
+            <li><a>Female</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary>Tennis</summary>
+          <ul>
+            <li><a>Male</a></li>
+            <li><a>Female</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+    </template>
+    
+    <script>
+    export default {
+      name: 'SideBar',
+    }
+    
+    // some methods to emit user selection to GamesPage.vue
+    </script>
