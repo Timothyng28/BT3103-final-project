f4be5470d99d0df877cdb2cc5408e8608bd3f998
tmotluke <timothyng28@gmail.com>
2025-02-28 18:34:10 +0800

    feat: add Description component

--- src/views/ResidencesPage/ResidencesPage.vue
+++ src/views/ResidencesPage/ResidencesPage.vue
@@ -1,12 +1,14 @@
 <template>
     <main class="w-screen">
       <Intro />
+      <Description />
     </main>
   </template>
   
   <script setup>
   import { onMounted } from 'vue'
   import Intro from './components/Intro.vue'
+  import Description from './components/Description.vue';
   
   onMounted(async () => {
     const LocomotiveScroll = (await import('locomotive-scroll')).default
