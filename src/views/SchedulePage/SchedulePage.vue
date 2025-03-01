0072a75aae82f3f08a4f462409de1728d8d6f9e6
tmotluke <timothyng28@gmail.com>
2025-03-01 12:10:23 +0800

    feat: add calendar to schedule page with opening modal on click

--- src/views/SchedulePage/SchedulePage.vue
+++ src/views/SchedulePage/SchedulePage.vue
@@ -1,6 +1,11 @@
 <template>
-schedule page
+    <div class="flex justify-center">
+<Calendar class = "w-[70%] h-[50%]"/>
+</div>
 </template>
 
 <script setup>
+import Calendar from './components/Calendar.vue';
+
+
 </script>
---
+++ src/views/SchedulePage/components/Calendar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+      <FullCalendar :options="calendarOptions" />
+  
+      <dialog id="eventModal" class="modal" ref="modalDialog">
+        <div class="modal-box">
+          <h3 class="text-lg font-bold">{{ modalData.title }}</h3>
+          <p class="py-4">Event Date: {{ modalData.date }}</p>
+          
+        </div>
+        <!-- This makes the backdrop clickable to close the modal -->
+        <form method="dialog" class="modal-backdrop">
+          <button>Close</button>
+        </form>
+      </dialog>
+    </div>
+  </template>
+  
+  <script setup>
+import { ref } from 'vue'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+const modalData = ref({
+  title: '',
+  date: ''
+})
+const modalDialog = ref(null)
+
+const openModal = () => {
+  if (modalDialog.value) {
+    modalDialog.value.showModal() 
+  }
+}
+
+const closeModal = () => {
+  if (modalDialog.value) {
+    modalDialog.value.close() 
+  }
+}
+
+const calendarOptions = ref({
+  plugins: [dayGridPlugin],
+  initialView: 'dayGridMonth',
+  events: [
+    { date: '2025-03-01', display: 'background' },
+    { date: '2025-03-15', display: 'background' }
+  ],
+  eventClick: function(info) {
+    // Set the modal data and open the modal
+    modalData.value.title = info.event.title
+    modalData.value.date = info.event.startStr
+    openModal()
+  },
+  fixedWeekCount: false
+})
+</script>
